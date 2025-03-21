cfdf6349dfa1f9af68c6952795c90e9c7f6b59d9
gopivasanthi <gopi.sivanappanvasanthi@emea.nttdata.com>
2025-03-21 03:33:32 +0000

    committing the changes for 20-03

--- Clubmates.Web/Areas/Club/Controllers/HomeController.cs
+++ Clubmates.Web/Areas/Club/Controllers/HomeController.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace Clubmates.Web.Areas.Club.Controllers
 {
-    [Area("Club")]
-    [Route("Club/[controller]/[action]")]
-    public class HomeController : Controller
+
+    public class HomeController : ClubBaseController
     {
-        public IActionResult Index()
+        public IActionResult Index(int? clubId = 0, string returnUrl = "/")
         {
+            if (!ModelState.IsValid)
+            {
+                return Redirect(returnUrl);
+            }
             return View();
         }
     }
