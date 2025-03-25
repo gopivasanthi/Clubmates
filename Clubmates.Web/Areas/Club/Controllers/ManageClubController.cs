596ac398fa26b97abfee0144c113434495c07734
gopivasanthi <gopi.sivanappanvasanthi@emea.nttdata.com>
2025-03-25 03:49:15 +0000

    24-03-pract

--- Clubmates.Web/Areas/Club/Controllers/ManageClubController.cs
+++ Clubmates.Web/Areas/Club/Controllers/ManageClubController.cs
@@ -1,10 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Clubmates.Web.AppDbContext;
+using Clubmates.Web.Areas.Club.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace Clubmates.Web.Areas.Club.Controllers
 {
-    public class ManageClubController : ClubBaseController
+    public class ManageClubController(IClubLayoutService clubLayoutService) : ClubBaseController
     {
-        public IActionResult Index()
+        private readonly IClubLayoutService _clubLayoutService = clubLayoutService;
+        public async Task<IActionResult> Index(int? clubId)
+        {
+
+            if (!ModelState.IsValid)
+                return Redirect("/Clubs/Index");
+
+            var loggedInUserEmail = HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+            if (loggedInUserEmail == null)
+                return Redirect("/Account/Login");
+            if (await _clubLayoutService.ValidateClubUser(loggedInUserEmail))
+                return Redirect("/Account/Login");
+
+            var clubLayout = await _clubLayoutService.PopulateClubLayout(loggedInUserEmail, clubId ?? 0);
+
+            ViewBag.MainMenuItems = clubLayout.MainMenus;
+            ViewBag.ImgSrc = clubLayout.Logo;
+
+            return View();
+        }
+        public async Task<IActionResult> ManageMembers()
         {
             return View();
         }
